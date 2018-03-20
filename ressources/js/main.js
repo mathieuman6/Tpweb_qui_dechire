1ff548713f50bea679d0d5f054a3f5a4cb48a688
mathieuman6 <mathieu.grimard@hotmail.fr>
2018-03-20 12:28:18 +0100

    bouton + sur le formulaire ingredient

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,11 +1,11 @@
 
-function pgcd(a,b) { // a>0, b>0  
-  do {  
-    var r=a;  
-    a=b;  
-    b=r%a;  
-  } while (b>0);  
-  return a;  
+function pgcd(a,b) { // a>0, b>0
+  do {
+    var r=a;
+    a=b;
+    b=r%a;
+  } while (b>0);
+  return a;
 }
 function plus_button(){
     $quantities = $(".quantity");
@@ -28,7 +28,7 @@ function plus_button(){
 		    }
 		    quant.firstChild.value = nums;
 		    nums = Math.round(nums*100)/100;
-		    quant.firstChild.data = nums.toString(); 
+		    quant.firstChild.data = nums.toString();
 	    });
 	    $(".nbpers")[0].innerHTML = ($val+1).toString();
     }
@@ -55,11 +55,44 @@ function minus_button(){
 		    }
 		    quant.firstChild.value = nums;
 		    nums = Math.round(nums*100)/100;
-		    quant.firstChild.data = nums.toString(); 
+		    quant.firstChild.data = nums.toString();
 	    });
 	    $(".nbpers")[0].innerHTML = ($val-1).toString();
     }
 }
 
-$(".plus-button")[0].addEventListener("click",plus_button,false);
-$(".minus-button")[0].addEventListener("click",minus_button,false);
+var $plusBtn=$('.plus-button');
+if($plusBtn.lenth > 0){
+  $(".plus-button")[0].addEventListener("click",plus_button,false);
+}
+
+var $plusBtn=$('.minus-button');
+if($plusBtn.lenth > 0){
+  $(".minus-button")[0].addEventListener("click",minus_button,false);
+}
+
+var $addIngredient = $('#boutonAddIngredient');
+if($addIngredient.length > 0){
+  $('#boutonAddIngredient').on('click', function(e) {
+    e.preventDefault();
+    console.dir($(this).parent());
+    var field=  $('<div class="columns is-mobile">'+
+      '<div class="column is-11 is-offset-1">'+
+        '<section class="formul">'+
+          '<div class="field has-addons has-addons-right">'+
+            '<p class="control">'+
+                '<input class="input" type="text" placeholder="Quantité" style="width:40%;">'+
+              '</span>'+
+            '</p>'+
+            '<p class="control">'+
+              '<input class="input" type="text" placeholder="ingredient">'+
+            '</p>'+
+          '</div>'+
+          '</section>'+
+        '</div>'+
+      '<div>');
+
+    $(this).parent().before(field);
+
+  });
+}
